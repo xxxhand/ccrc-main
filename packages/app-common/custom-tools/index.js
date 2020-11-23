206136f7d3ee76d2fbaeed5c3e35b5dc04f26484
xxxhand <nohandboy@gmail.com>
2020-11-23 11:25:48 +0800

    Add custom validator object

--- packages/app-common/custom-tools/index.js
+++ packages/app-common/custom-tools/index.js
@@ -2,4 +2,5 @@ module.exports = {
   customArgvs: require('./custom-argvs'),
   customRequestTracer: require('./custom-request-tracer'),
   customLogger: require('./custom-logger'),
+  CustomValidator: require('./custom-validator'),
 };
